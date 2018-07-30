4af384f62c500c323f7d20d3d08edfd61866e2bf
Derek Hammer and James Hanley <james@>
2018-07-30 12:53:50 -0700

    Add outline for vowels

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,31 @@
-export function entry(bodyEntry){
-  var splitArray = bodyEntry.split(" ");
-  var outputWordsNumber = splitArray.length;
+export function WordCount(bodyEntry) {
+  let splitArray = bodyEntry.split(" ");
+  let outputWordsNumber = splitArray.length;
   //method for returning number of words
   return outputWordsNumber;
   //seperate method for returning number of vowels and consonants
-  
+
   //seperate method getTeaser to return first sentence of entry.
+}
+export function VowelsCount(bodyEntry) {
+  //
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let splitArray = bodyEntry.split("");
+  let x = 0;
+  splitArray.forEach(function(letter){
+    vowels.forEach(function(vowel){
+      if(letter === vowel){
+        x += 1;
+      };
+    });
+  });
+  return x;
+}
+export function Consonants(bodyEntry) {
+  let x = 0;
+  return x;
+}
+export function ReturnSentence(bodyEntry) {
+  var outputSentence = [bodyEntry];
+  return outputSentence;
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { entry } from './entry.js';
+import { WordCount, VowelsCount, Consonants, ReturnSentence } from './entry.js';
 $(document).ready(function(){
   $('#journal-form').submit(function(event){
     event.preventDefault();
-    var bodyEntry = $('#bodyEntry').val();
-    var wordsNumber = entry(bodyEntry);
-    $('#wordsNumber').append("<li>" + wordsNumber + "</li>");
+    const bodyEntry = $('#bodyEntry').val();
+    let wordsNumber = WordCount(bodyEntry);
+    let vowelsNumber = VowelsCount(bodyEntry);
+    $('#wordsNumber').append("<li> Total amount of words in sentence:" + wordsNumber + "</li>");
+    $('#vowelsNumber').append("<li>Total amount of vowels: "+ vowelsNumber + "</li>");
   });
 });
